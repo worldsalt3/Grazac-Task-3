9afa57241a934afa7f5df77a3452e1e1ccdd183a
Worldsalt3 <riliwanhazzan@gmail.com>
2021-03-26 14:09:24 +0100

    build countdown interface

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+
+  const [second, setSecond] = useState('00')
+  const [minute, setMinute] = useState('00')
+  const [isActive, setIsActive] = useState(false)
+  const [counter, setCounter] = useState(0)
+
   return (
-    <div className="App">
-      
+    <div className='App'>
+      <div className='time'>
+        <span className='minute'>{minute}</span> <span>:</span>{' '}
+        <span className='seconds'>{second}</span>
+      </div>
+      <div className='button'></div>
     </div>
-  );
+  )
 }
 
 export default App;
